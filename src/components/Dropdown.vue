3803c39ceeb64a8120c4ffb387d3472a3a67e735
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2019-11-12 22:19:13 +0100

    dodao linkove i svg ikonicu

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,25 +1,25 @@
 <template>
-<ul class="list list_menu list_menu_level_2"
+<ul class="list list_menu list_menu_level_2 bg_dark"
 	v-show="list.open">
 	<li class="list__item list_item_sidebar"
 		v-for="(item, index) in list.subnav"
 		:key="'item'+index">
-		<div class="shell posrel">
-			<a :href=item.title class="link link_menu">
+		<div class="shell posrel d_flex align_items_center justify_content_space_between padding_10">
+			<a :href=item.title class="link link_menu color_light d_flex align_items_center">
 				{{ item.title }}
 			</a>
 			<span v-if="item.subnav"
-				class="icon"
+				class="icon icon_dropdown_toggle d_flex rounded_9999"
 				@click="item.open = !item.open"
-				:class="{'icon-open': !item.open,
-						'icon-closed': item.open}">
-				<svg class="svg svg_icon w100_h100" viewBox="0 0 50 50">
+				:class="{'icon-closed': !item.open,
+						'icon-open': item.open}">
+				<svg class="svg svg_icon w100 h100" viewBox="0 0 50 50">
 					<rect class="transform_origin_center scales_y transition_primary" width="2" height="20" x="24" y="15"></rect>
 					<rect class="" width="20" height="2" x="15" y="24"></rect>
 				</svg>
 			</span>
 		</div>
-		<Dropdown class="dropdown"
+		<Dropdown class="dropdown dropdown_level_2"
 			v-if="item.subnav"
 			:list="item" />
 	</li>
@@ -35,28 +35,13 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.w100_h100 {
-	width: 100%;
-	height: 100%;
-}
-.posrel {
-	position: relative;
-}
-.transform_origin_center {
-	transform-origin: center;
-}
-.transition_primary {
-	transition: .28s ease-in-out;
-}
-
+<style lang="scss" >
 .list {
 	list-style: none;
+	padding-left: 0;
 }
 .list_menu {}
 .list_menu_level_2 {
-	color: #fefefe;
-	background-color: #333;
 	border-top: 1px solid #888;
 }
 
@@ -73,30 +58,72 @@ export default {
 
 .link {}
 .link_menu {
-	padding: 10px 0;
-	text-indent: 20px;
-	display: block;
-	color: #fff;
+	min-height: 40px;
 }
 
 .icon {
 	cursor: pointer;
-	position: absolute;
-	top: 0;
-	right: 0;
-	height: 100%;
-	display: block;
 	background-color: #39df5f;
-	width: 40px;
+}
+.icon_dropdown_toggle {
+	min-width: 40px;
+	max-width: 40px;
 }
 .icon-open {
-	background-color: #df0489;
+	background-color: #dbcf5d;
 	.scales_y {
 		transform: scaleY(0);
 	}
 }
 .icon-closed {
-	background-color: #dbcf5d;
+	background-color: #df0489;
 }
+//
+// modifiers
+//
+.w100 {
+	width: 100%;
+}
+.h100 {
+	height: 100%;
+}
+.posrel {
+	position: relative;
+}
+.d_flex {
+	display: flex;
+}
+.justify_content_space_between {
+	justify-content: space-between;
+}
+.justify_content_center {
+	justify-content: center;
+}
+.align_items_center {
+	align-items: center;
+}
+.padding_10 {
+	padding: 10px;
+}
+.rounded_9999 {
+	border-radius: 9999px;
+}
+.bg_dark {
+	background-color: #333;
+}
+.color_light {
+	color: #fff;
+}
+.color_dark {
+	color: #000;
+}
+
+.transform_origin_center {
+	transform-origin: center;
+}
+.transition_primary {
+	transition: .28s ease-in-out;
+}
+
 </style>
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,16 +5,16 @@
 		<li v-for="(item, index) in navigation"
 			class="list__item list_item_sidebar"
 			:key="'item'+index">
-			<div class="shell posrel">
-				<a :href="item.title" class="link link_menu">
+			<div class="shell posrel d_flex align_items_center justify_content_space_between padding_10">
+				<a :href="item.title" class="link link_menu color_dark d_flex align_items_center">
 					{{ item.title }}
 				</a>
 				<span v-if="item.subnav"
-					class="icon"
+					class="icon icon_dropdown_toggle d_flex rounded_9999"
 					@click="item.open = !item.open"
-					:class="{'icon-open': !item.open,
-							'icon-closed': item.open}">
-					<svg class="svg svg_icon w100_h100" viewBox="0 0 50 50">
+					:class="{'icon-closed': !item.open,
+							'icon-open': item.open}">
+					<svg class="svg svg_icon w100 h100" viewBox="0 0 50 50">
 						<rect class="transform_origin_center scales_y transition_primary" width="2" height="20" x="24" y="15"></rect>
 						<rect class="" width="20" height="2" x="15" y="24"></rect>
 					</svg>
@@ -46,76 +46,12 @@
 </script>
 
 <style lang="scss" scoped>
-.w100_h100 {
-	width: 100%;
-	height: 100%;
-}
-.posrel {
-	position: relative;
-}
-.transform_origin_center {
-	transform-origin: center;
-}
-.transition_primary {
-	transition: .28s ease-in-out;
-}
-
 .sidebar {
 	width: 100%;
 	max-width: 350px;
 	min-height: 100vh;
 	background-color: #fefefe;
 	box-shadow: 10px 0 20px rgba($color: #000, $alpha: .2)
-}
-
-#navigation {
-	padding: 0;
-	list-style: none;
-	border-bottom: 1px solid #dedede;
-	
-	.list {
-		list-style: none;
-	}
-	.list_menu {}
-
-	.list__item {}
-	.list_item_sidebar {
-		color: #555;
-		border-top: 1px solid #dedede;
-	}
-
-	.dropdown {}
-	.dropdown_level_2 {
-		padding-left: 20px;
-	}
-
-	.link {}
-	.link_menu {
-		padding: 10px 0;
-		text-indent: 20px;
-		display: block;
-		color: #000;
-	}
-
-	.icon {
-		cursor: pointer;
-		position: absolute;
-		top: 0;
-		right: 0;
-		height: 100%;
-		display: block;
-		background-color: #39df5f;
-		width: 40px;
-	}
-	.icon-open {
-		background-color: #df0489;
-		.scales_y {
-			transform: scaleY(0);
-		}
-	}
-	.icon-closed {
-		background-color: #dbcf5d;
-	}
 }
 </style>
 
